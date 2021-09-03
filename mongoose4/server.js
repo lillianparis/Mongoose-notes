64b47bb1f193a70433cb7bd04e9584721a62df0d
Lillian Paris <lillian.paris529@gmail.com>
2021-09-03 08:22:33 -0700

    added

--- mongoose4/server.js
+++ mongoose4/server.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-=======
-// npm install express
-// npm install mongoose
->>>>>>> 3abc164d26d0f3b8b510b5d4015ac71db0e88d39
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -17,7 +12,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Mongoose onnection creating
-<<<<<<< HEAD
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/shopping", {
   useNewUrlParser: true,
   useFindAndModify: false
@@ -32,17 +27,4 @@ app.listen(PORT, () => {
 });
 
 
-=======
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/car", {
-   useNewUrlParser: true,
-   useFindAndModify: false
-});
 
-let db = mongoose.connection;
-db.once("open", () => console.log("connected to database"));
-
-
-app.listen(PORT, () => {
-   console.log(`App running on port ${PORT}!`);
-});
->>>>>>> 3abc164d26d0f3b8b510b5d4015ac71db0e88d39
