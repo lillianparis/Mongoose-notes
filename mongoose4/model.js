3abc164d26d0f3b8b510b5d4015ac71db0e88d39
Lillian Paris <lillian.paris529@gmail.com>
2021-09-02 17:36:13 -0700

    added

--- mongoose4/model.js
+++ mongoose4/model.js
@@ -1,49 +1,29 @@
+// Required to use mongoose
 const mongoose = require('mongoose');
+
+// Creating schema
 const Schema = mongoose.Schema;
 
-// The car schema contains several properties of type string. It contains a reference to the dealerships schema. The car Schema contains an array of dealerships. Each dealership consists of a description, brand and location.
-// Mongoose enforces flexibility to create schemas with references to other chemas or, as in the example with the dealerships property. You're allowed to create an array of child properties that could be contained in a related schema. 
-// When a car
+// Creating carSchema
 
-var vehicleSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
+var carSchema = new Schema({
     name: {
-            firstName: String,
-        lastName: String
+        brandName: String,
+        modelName: String
     },
-    biography: String,
-    twitter: String,
-    facebook: String,
-    linkedin: String,
-    profilePicture: Buffer,
-    created: { 
-        type: Date,
-        default: Date.now
+    value: {
+    engine: String,
+    description: String,
+    mpg: String,
+    year: String,
+},
+    date: {
+    type: Date,
+    default: Date.now
     }
 });
 
-const carSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    carMake: String,
-    carmodel: String,
-    country: {
-        type: mongoose.Schema.Types.ObjectId
-    },
-    dealerships: [
-        {
-            description: String,
-            brands: String,
-            location: String,
-            Created: {
-                type: Date,
-                default: Date.now
+// Making our model
+const Car = mongoose.model("Car", carSchema);
 
-            }
-        }
-    ],
-    created: {
-        type: Date,
-        default: Date.now
-    }
-});
-const Car = mongoose.model('Car', carSchema);
+module.exports = Car;
--- mongoose4/server.js
+++ mongoose4/server.js
@@ -1,9 +1,28 @@
-const mongoose = require('mongoose');
- 
-mongoose.connect('mongodb://localhost/mongoose_dog', function (err) {
- 
-   if (err) throw err;
- 
-   console.log('Successfully connected');
- 
+// npm install express
+// npm install mongoose
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Specify the port 
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// Mongoose onnection creating
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/car", {
+   useNewUrlParser: true,
+   useFindAndModify: false
+});
+
+let db = mongoose.connection;
+db.once("open", () => console.log("connected to database"));
+
+
+app.listen(PORT, () => {
+   console.log(`App running on port ${PORT}!`);
 });
