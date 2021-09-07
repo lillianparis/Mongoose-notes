e31116f6f94f2062d1d4d95913449cd3f182c33f
Lillian Paris <lillian.paris529@gmail.com>
2021-09-07 09:40:54 -0700

    Editing

--- mongoose3/model.js
+++ mongoose3/model.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
-
-// Creating the schema
 const Schema = mongoose.Schema;
 
-// Creating the transaction Schema
-// Enter the transaction name, amount and the date is going to be saved as the current date of deposit or withdrawl
+
 const peopleSchema = new Schema({
   name: {
     firstName: String,
@@ -19,7 +16,6 @@ const peopleSchema = new Schema({
   }
 });
 
-// Making our model
 const People = mongoose.model("People", peopleSchema);
 
 module.exports = People;
